f8f3007a02e8c0d53baa812035e6fdd961fd5e74
fedverdev <fedia.verbitsky@yandex.ru>
2025-12-27 16:10:28 +0700

    update docs

---
+++ src/main/java/com/github/fedverdev/session/Member.java
@@ -0,0 +1,37 @@
+package com.github.fedverdev.session;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.net.InetAddress;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    private InetAddress address;
+    private int portUDP;
+    private int portTCP;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        if (this.address.getHostName().equals(member.address.getHostName())) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(address);
+    }
+}
+
+
---
+++ src/main/java/com/github/fedverdev/session/Session.java
@@ -0,0 +1,5 @@
+package com.github.fedverdev.session;
+
+public class Session {
+
+}
